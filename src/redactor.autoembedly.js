5b424d59dbd49c14fd2c5994a3b5f51f0ffbb999
gpbmike <gpbmike@gmail.com>
2013-08-30 20:30:36 -0700

    auto embedly plugin
    automatically change links into embeds (regex for youtube and vimeo
    only right now)

---
+++ src/redactor.autoembedly.js
@@ -0,0 +1,59 @@
+/*
+ * webhook-redactor
+ *
+ *
+ * Copyright (c) 2013 Mike Horn
+ * Licensed under the MIT license.
+ */
+
+(function ($) {
+  "use strict";
+
+  window.RedactorPlugins = window.RedactorPlugins || {};
+
+  // namespacing
+  var AutoEmbedly = function (redactor) {
+    this.redactor = redactor;
+    this.observe();
+  };
+  AutoEmbedly.prototype = {
+    observe: function () {
+      this.redactor.$editor.on('keyup.redactor', $.proxy(function (event) {
+        if (event.which === this.redactor.keyCode.ENTER) {
+          this.checkNode(this.redactor.$editor.get(0));
+        }
+      }, this));
+    },
+    // recursively check node and children for supported links
+    checkNode: function (node) {
+      $.each(node.childNodes, $.proxy(function (index, node) {
+        if (node.nodeType === 3 && node.nodeValue && node.nodeValue.match(/((http:\/\/(.*youtube\.com\/watch.*|.*\.youtube\.com\/v\/.*|youtu\.be\/.*|.*\.youtube\.com\/user\/.*|.*\.youtube\.com\/.*#.*\/.*|m\.youtube\.com\/watch.*|m\.youtube\.com\/index.*|.*\.youtube\.com\/profile.*|.*\.youtube\.com\/view_play_list.*|.*\.youtube\.com\/playlist.*|www\.vimeo\.com\/groups\/.*\/videos\/.*|www\.vimeo\.com\/.*|vimeo\.com\/groups\/.*\/videos\/.*|vimeo\.com\/.*|vimeo\.com\/m\/#\/.*|player\.vimeo\.com\/.*))|(https:\/\/(.*youtube\.com\/watch.*|.*\.youtube\.com\/v\/.*|www\.vimeo\.com\/.*|vimeo\.com\/.*|player\.vimeo\.com\/.*)))/i)) {
+
+          var url = node.nodeValue,
+              shiv = $('<span>loading...</span>');
+
+          $(node).replaceWith(shiv);
+
+          $.embedly.oembed(url).done(function (results) {
+            $.each(results, function () {
+              shiv.replaceWith(this.html);
+            });
+          });
+
+        } else if (node.nodeType === 1 && !/^(a|button|textarea)$/i.test(node.tagName)) {
+          this.checkNode(node);
+        }
+      }, this));
+
+      return this.matches;
+    }
+  };
+
+  // Hook up plugin to Redactor.
+  window.RedactorPlugins.autoembedly = {
+    init: function () {
+      this.autoembedly = new AutoEmbedly(this);
+    }
+  };
+
+}(jQuery));
